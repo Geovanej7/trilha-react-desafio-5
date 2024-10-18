e2b606d6c1e2ea3a04cdbe1dce5242c8510a6f13
Geovane Júnior <122648367+Geovanej7@users.noreply.github.com>
2024-10-18 19:41:10 -0300

    Update api.js

--- services/api.js
+++ services/api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 export const api  = axios.create({
-    baseURL: 'https://riomiodlxskcqriigpnx.supabase.co/rest/v1',
+    baseURL: '',
     headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpb21pb2RseHNrY3FyaWlncG54Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkxMTA1MTksImV4cCI6MjA0NDY4NjUxOX0.t2Po9_MnQTGPtVe62oQBOvGZztgcc2tuqk-2SbMHQBA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpb21pb2RseHNrY3FyaWlncG54Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTExMDUxOSwiZXhwIjoyMDQ0Njg2NTE5fQ.ockkwuSxKVhMkAerj-aiQSTIqbqtF4xJUTCysoi3_PI"
+        apikey: "",
+        authorization: "Bearer"
     }
 })
